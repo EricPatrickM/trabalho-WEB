cbcbd5d36b419d242f2055cd09bf11b3d64e6f65
EricPatrickM <ericmili.m7@gmail.com>
2023-03-04 12:22:54 -0300

    trabalho 3

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import customersRouter from "@modules/customer/routes/customer.routes";
 import publishRouter from "@modules/publish/routes/publishRoutes";
 import passwordRouter from "@modules/users/routes/password.routes";
 import profileRouter from "@modules/users/routes/profile.routes";
@@ -12,5 +13,6 @@ routes.use('/user', usersRouter)
 routes.use('/sessions', sessionsRouter)
 routes.use('/password', passwordRouter)
 routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
